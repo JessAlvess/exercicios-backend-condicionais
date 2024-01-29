0d6783e2f5ee69a5ff260930f49f92e618977ec1
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 20:08:29 -0300

    feat: exercicio 7 finalizado

--- 07/index.js
+++ 07/index.js
@@ -3,3 +3,11 @@ const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
 //seu código aqui
+
+const aferirRenda = () => {
+
+const rendimento = (totalDeRendimentos > 2855970) ? console.log('PEGA LEAO') : console.log('VAZA LEAO! JA TA DIFICIL SEM VOCE');
+
+}
+
+const result = (aposentada || portadoraDeDoenca) ? console.log('ISENTA') : aferirRenda();
