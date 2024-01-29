76fc5e785ad7e7a2687919b5015e67ccea40bde5
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 20:15:15 -0300

    feat: exercicio 8 finalizado

--- 08/index.js
+++ 08/index.js
@@ -3,3 +3,12 @@ const possuiPatologia = false;
 const altura = 180;
 const ehEstudante = false;
 
+const verificarPreco = () => {
+    if (ehEstudante || idade < 18) {
+        console.log('10 REAIS');
+    } else {
+        console.log('20 REAIS');
+    }
+}
+
+const result  = (possuiPatologia || altura < 150 || idade < 12 || idade > 65) ? console.log('ACESSO NEGADO') : verificarPreco();
