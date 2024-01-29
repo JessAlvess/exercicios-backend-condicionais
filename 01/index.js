2ae80995f42465dd84b82e1a51a75ecd190f3af5
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 19:27:34 -0300

    feat: exercicio 1 finalizado

--- 01/index.js
+++ 01/index.js
@@ -2,3 +2,10 @@ const jogada1 = "pedra"
 const jogada2 = "tesoura"
 
 //seu código aqui
+if (jogada1 === 'pedra' && jogada2 === 'tesoura' || jogada1 === 'tesoura' && jogada2 === 'papel' || jogada1 === 'papel' && jogada2 === 'pedra') {
+    console.log(`${jogada1} venceu!!!`);
+} else if (jogada2 === 'pedra' && jogada1 === 'tesoura' || jogada2 === 'tesoura' && jogada1 === 'papel' || jogada2 === 'papel' && jogada1 === 'pedra') {
+    console.log(`${jogada2} venceu!!!`);
+} else {
+    console.log('Empatou');
+}
