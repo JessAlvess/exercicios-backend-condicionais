9a35575ab73c346e723723d1786ceb77767b2a1b
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 20:20:49 -0300

    feat: exercicio 9 finalizado

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,13 @@
-const nota = 8.5;
+const nota = 3.9;
+
+if (nota < 4) {
+    console.log('O estudando obteve conceito E');
+} else if (nota >= 4 && nota < 6) {
+    console.log('O estudando obteve conceito D');
+} else if (nota >= 6 && nota < 8) {
+    console.log('O estudando obteve conceito C');
+} else if (nota >= 8 && nota < 9) {
+    console.log('O estudando obteve conceito B');
+} else {
+    console.log('O estudando obteve conceito A');
+}
