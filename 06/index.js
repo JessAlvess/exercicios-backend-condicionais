c758607c0a5dcdbe26196c6197ea5ad8378c0826
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 20:00:30 -0300

    feat: exercicio 6 finalizado

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,17 @@
-const alturaEmCm = 185;
+const alturaEmCm = 205;
 
 //seu código aqui
+const position = (altura) => {
+  
+if (altura <= 185) {
+    console.log('Líbero');
+} else if (altura >= 186 && altura < 195) {
+    console.log('Ponteiro');
+} else if (altura >= 196 && altura < 205) {
+    console.log('Oposto');
+} else{
+    console.log('Central');
+}
+}
+
+const result = (alturaEmCm >= 180) ? position(alturaEmCm) : console.log('Reprovado');
