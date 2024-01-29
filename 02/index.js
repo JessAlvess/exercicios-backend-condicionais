35d2353db119c365913b4dd257a85e2be311896f
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 19:31:51 -0300

    feat: exercicio 2 finalizado

--- 02/index.js
+++ 02/index.js
@@ -1,4 +1,9 @@
 const jogada1 = 5;
-const jogada2 = 3;
+const jogada2 = 2;
 
 //seu código aqui
+if ((jogada1 + jogada2) % 2 === 0) {
+    console.log('Par vence');
+} else {
+    console.log('Ímpar vence');
+}
