cc6cd14d65af8114d9ca91a0218b2d24b74b083e
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 21:17:02 -0300

    feat: exercicio 13 finalizado

--- 13/index.js
+++ 13/index.js
@@ -1,6 +1,26 @@
 //tipo de pagamento (dinheiro, credito, debito, cheque).
-const tipoDePagamento = "credito";
+const tipoDePagamento = "cheque";
 
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+switch (tipoDePagamento) {
+    case 'credito':
+        console.log(`Valor a ser pago: R$${((valorDoProduto - (valorDoProduto * (5/100)))/100).toFixed(2)}`);
+        break;
+
+    case 'cheque':
+        console.log(`Valor a ser pago: R$${((valorDoProduto - (valorDoProduto * (3/100)))/100).toFixed(2)}`);
+    break;
+
+    case 'debito':
+        console.log(`Valor a ser pago: R$${((valorDoProduto - (valorDoProduto * (10/100)))/100).toFixed(2)}`);
+    break;
+
+    case 'dinheiro':
+        console.log(`Valor a ser pago: R$${((valorDoProduto - (valorDoProduto * (10/100)))/100).toFixed(2)}`);
+        break;
+
+    default:
+        break;
+}
