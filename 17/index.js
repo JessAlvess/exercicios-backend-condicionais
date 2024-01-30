d1eb9a56736424047058b8e2b68478f7c3f4b6a4
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 21:59:29 -0300

    feat: exercicio 17 finalizado

--- 17/index.js
+++ 17/index.js
@@ -5,4 +5,10 @@ const valorDoProduto = 100000;
 const quantidadeDoParcelamento = 10;
 
 //valor pago
-const valorPago = 300;
+const valorPago = 500;
+
+let valorParcela = valorDoProduto / quantidadeDoParcelamento;
+
+let parcelasPagas = valorPago / valorParcela;
+
+console.log(`Restam ${(quantidadeDoParcelamento - (parcelasPagas*100)).toFixed(0)} parcelas de R$${(valorParcela/100).toFixed(2)}`);
