e005590ec30853bc0cb9ae4e9a0fc85a59e1013c
Jessé Alves <jessealvespr@outlook.com>
2024-01-29 21:29:36 -0300

    feat: exercicio 15 finalizado

--- 15/index.js
+++ 15/index.js
@@ -1,4 +1,11 @@
-const primeiroNome = "Mario";
-const sobrenome = "";
+const primeiroNome = "Super";
+const sobrenome = "Mario";
 const apelido = "";
 
+if (apelido) {
+    console.log(apelido);
+} else if (sobrenome) {
+    console.log(primeiroNome + ' ' + sobrenome);
+} else {
+    console.log(primeiroNome);
+}
